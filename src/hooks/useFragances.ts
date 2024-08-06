14c23460825ad783e934de7454da8d3f2c5e1801
Jorge Morales <luifer991@gmail.com>
2024-08-05 19:51:10 -0500

    changing axios for simple fetching, and adding pagination, TO REMENBER: create and api pagination

--- src/hooks/useFragances.ts
+++ src/hooks/useFragances.ts
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+const url = new URL('https://66a3994f44aa63704581d661.mockapi.io/api/products/fragrances')
+url.searchParams.append('page', '1')
+url.searchParams.append('limit', '8')
 
 // Define la interfaz para el producto
 interface Fragrance {
@@ -14,30 +17,34 @@ interface Fragrance {
 
 // Define la interfaz para el estado del hook
 interface UseFragrancesState {
-    fragrances: Fragrance[];
-    loading: boolean;
-    error: string | null;
+    fragrances: Fragrance[]
+    loading: boolean
+    error: string | null
 }
 
 export const useFragrances = (): UseFragrancesState => {
-    const [fragrances, setFragrances] = useState<Fragrance[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+    const [fragrances, setFragrances] = useState<Fragrance[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
-        const fetchFragrances = async () => {
-            try {
-                const response = await axios.get<Fragrance[]>('https://66a3994f44aa63704581d661.mockapi.io/api/products/fragrances');
-                setFragrances(response.data);
-                setLoading(false);
-            } catch (err) {
-                setError('Error fetching fragrances');
-                setLoading(false);
-            }
-        };
+        setLoading(true)
+        setError(null)
 
-        fetchFragrances();
-    }, []);
+        fetch(url,{
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        }).then(async res => {
+            if (!res.ok) throw new Error('Error al obtener los datos')
+            return await res.json()
+        }).then(res => {
+            setFragrances(res)
+        }).catch(err => {
+            setError(err.message)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
 
-    return { fragrances, loading, error };
+    return { fragrances, loading, error }
 };
