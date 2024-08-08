9f4d684271d24582d5f20e84ca54bc9905327d1b
Jorge Morales <luifer991@gmail.com>
2024-08-08 13:15:50 -0500

    refactoring products making condicional rendering

--- src/UI/Products.tsx
+++ src/UI/Products.tsx
@@ -3,16 +3,7 @@ import '../components/styles/products.css'
 import { useFragrances } from '../hooks/useFragances';
 
 export function Products() {
-
     const { fragrances, loading, error } = useFragrances();
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-
-    if (error) {
-        return <div>{error}</div>;
-    }
 
     return (
         <>
@@ -21,22 +12,27 @@ export function Products() {
                 subtitle="Productos Destacados"
                 body="Explora nuestra gama de perfumes, velas y productos de cuidado personal. Cada uno hecho a la medida para ti."
             />
-            <ul className='container__list'>
-                {fragrances.map(f => (
-                    <section key={f.id} className='card__container'>
-                        <img src={f.image} alt={f.product_name} />
-                        <div className='li'>
-                            <li>{f.product_name}</li>
-                            <li>${f.price}</li>
-                            <li>{f.popularity}</li>
-                        </div>
-                    </section>
-                ))}
-            </ul>
-            <div className='container__btn'>
-                <a href="#" className='more__btn'>ver más</a>
-            </div>
+            {loading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {!loading && !error && (
+                <>
+                    <ul className='container__list'>
+                        {fragrances.map(f => (
+                            <section key={f.id} className='card__container'>
+                                <img src={f.image} alt={f.product_name} />
+                                <div className='li'>
+                                    <li>{f.product_name}</li>
+                                    <li>${f.price}</li>
+                                    <li>{f.popularity}</li>
+                                </div>
+                            </section>
+                        ))}
+                    </ul>
+                    <div className='container__btn'>
+                        <a href="#" className='more__btn'>ver más</a>
+                    </div>
+                </>
+            )}
         </>
-
     )
 }
